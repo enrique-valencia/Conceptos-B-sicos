69a5b76fbccfef250f0f11db689d97db382d6084
enrique-valencia <79728428+enrique-valencia@users.noreply.github.com>
2021-03-19 17:28:31 -0600

    Add files via upload

---
+++ Automovil.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Ico.Fes;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author 52553
+ */
+public class Automovil {
+    private String marca;
+    private String subMarca;
+    private int modelo;
+    private Color color;
+
+    public Automovil(String marca, String subMarca, int modelo, Color color) {
+        this.marca = marca;
+        this.subMarca = subMarca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public Automovil() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSubMarca() {
+        return subMarca;
+    }
+
+    public void setSubMarca(String subMarca) {
+        this.subMarca = subMarca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" + "marca=" + marca + ", subMarca=" + subMarca + ", modelo=" + modelo + ", color=" + color + '}';
+    }
+    
+    
+}
